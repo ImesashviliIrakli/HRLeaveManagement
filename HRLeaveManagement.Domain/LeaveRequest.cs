e5dcc322b4d398f544ee64f2c87aa25c33a62f1a
ImesashviliIrakli <imesashviliirakli5@gmail.com>
2024-01-23 01:30:19 +0400

    Add project files.

---
+++ HRLeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿using HRLeaveManagement.Domain.Common;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveRequest : BaseEntity
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime DateRequested { get; set; }
+    public string? RequestComments { get; set; }
+    public bool Approved { get; set; }
+    public bool Cancelled { get; set; }
+    public string RequestingEmployeeId { get; set; } = string.Empty;
+    public int LeaveTypeId { get; set; }
+    public LeaveType? LeaveType { get; set; }
+}
