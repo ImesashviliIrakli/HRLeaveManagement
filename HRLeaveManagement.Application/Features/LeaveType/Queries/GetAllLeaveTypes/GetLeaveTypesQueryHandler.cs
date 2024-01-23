1b87fe7e2da7e7b79253bcd7143ec4eb5ef79c3d
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2024-01-23 20:56:50 +0400

    Working on the application project

---
+++ HRLeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistance;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _repository;
+
+    public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        var leaveTypes = await _repository.GetAsync();
+
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+        return data;
+    }
+}
